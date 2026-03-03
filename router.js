13652162c01e71c61611299736d03c258596a43f
manuel perez <manuelperez.0000@gmail.com>
2026-03-03 09:01:02 -0400

    vercel json aded

--- router.js
+++ router.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors'
 import push from './routes/push.js';
 
+const testRoute = (req, res) => res.json({ message: "API de Giro Rides" });
+
 const route = express.Router();
-const router = (app)=>{
-    
-    app.use('/api/v1', route);
+const router = (app) => {
+
     app.use(cors())
 
+    app.use('/api/v1', route);
+    route.get('/', testRoute);
     route.use('/push', push);
 }
 
